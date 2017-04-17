aa636a1760ad84e7d25dee9c1a2b61946bf27ae8
ison.apple <ison@apple.com>
2017-04-17 18:23:51 +0800

    asdf

--- MyApp/app/src/main/java/com/ison/myapp/MainActivity.java
+++ MyApp/app/src/main/java/com/ison/myapp/MainActivity.java
@@ -2,12 +2,57 @@ package com.ison.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.myarray,android.R.layout.simple_list_item_multiple_choice );
+        ListView list_test = (ListView) findViewById(R.id.list_test1);
+        list_test.setAdapter(adapter);
+        */
+
+        /*
+        //要显示的数据
+        String[] strs = {"基神","B神","翔神","曹神","J神"};
+        //创建ArrayAdapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, strs);
+        //获取ListView对象，通过调用setAdapter方法为ListView设置Adapter设置适配器
+        ListView list_test = (ListView) findViewById(R.id.list_test1);
+        list_test.setAdapter(adapter);
+        */
+
+        String[] names = new String[]{"A神", "基神", "曹神"};
+        String[] says = new String[]{"无形被黑，最为致命", "大神好厉害~", "我将带头~"};
+        int[] imgIds = new int[]{R.mipmap.touxiang1, R.mipmap.touxiang2, R.mipmap.touxiang3};
+
+        List<Map<String, Object>> listitem = new ArrayList<Map<String, Object>>();
+        for (int i = 0; i < names.length; i++) {
+            Map<String, Object> showitem = new HashMap<String, Object>();
+            showitem.put("touxiang", imgIds[i]);
+            showitem.put("name", names[i]);
+            showitem.put("says", says[i]);
+            listitem.add(showitem);
+        }
+
+        //创建一个simpleAdapter
+        SimpleAdapter myAdapter = new SimpleAdapter(getApplicationContext(), listitem, R.layout.list_item, new String[]{"touxiang", "name", "says"}, new int[]{R.id.imgtou, R.id.name, R.id.says});
+        ListView listView = (ListView) findViewById(R.id.list_test);
+        listView.setAdapter(myAdapter);
+
     }
+
 }
